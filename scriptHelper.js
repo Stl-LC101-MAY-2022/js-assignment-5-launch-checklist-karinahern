1b2459f9192afd4d15d3678d943dd35d80951dea
Karina Hernandez <kmhernandez000@gmail.com>
2022-08-03 21:14:06 -0500

    Updated all the functions in scriptHelper.js and moved event listeners to script.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -14,11 +14,22 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+    let planets = getElementById('missionTarget');
+    planets.innerHTML = 
+    `<h2>Mission Destination</h2>
+    <ol>
+        <li>Name: ${name} </li>
+        <li>Diameter: ${diameter} </li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance} </li>
+        <li>Number of Moons: ${moons} </li>
+    </ol>
+    <img src="${imageUrl}">`
 }
 
 
 function validateInput(testInput) {
-    if(testInput === Number){
+    if(isNaN(testInput) === false){
         return "Is a number";
     }
     else if(isNaN(testInput) === true){
@@ -31,27 +42,18 @@ function validateInput(testInput) {
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    window.addEventListener('load', function(){
-        let form = document.querySelector('testForm');
-        form.addEventListener('sumbit', function(event) {
             
-            let pilot = document.querySelector("input[name=pilotName").value;
-            let copilot = document.querySelector("input[name=copilotName").value;
-            let fuelLevel = document.querySelector("input[name=fuelLevel").value;
-            let cargoLevel = document.querySelector("input[name=cargoMass").value;
-
-            let items = document.getElementById('faultyItems');
             let launchStatus = document.getElementById('launchStatus');
             let fuelStatus = document.getElementById('fuelStatus');
             let cargoStatus = document.getElementById('cargoStatus');
             let ready = true;
 
-            if (pilot === '' || copilot === '' || fuelLevel === '' || cargoLevel === ''){
+            if (validateInput(pilot) === 'Empty' || validateInput(copilot) === 'Empty' || validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty'){
                 alert('All fields are required!');
-                event.preventDefault(); }
+                preventDefault(); }
             if(validateInput(pilot) === 'Is a number' || validateInput(copilot) === 'Is a number' || validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number'){
                 alert('Make sure to enter valid information for each field!');
-                event.preventDefault();
+                preventDefault();
             } else {
                 items.style.visibility = 'visible';
 
@@ -70,17 +72,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
                 if(ready){
                     launchStatus.style.color = 'green';
                     launchStatus.innerHTML = 'Shuttle is ready for launch'
-                    myFetch();
-                    pickPlanet();
+            
                 } else {
                     items.style.visibility = 'visible';
                     launchStatus.style.color = 'red';
                     launchStatus.innerHTML = 'Shuttle not ready for launch';
                 }
             }
-        });
-    });
-}
+        };
+
 
 async function myFetch() {
     let planetsReturned;
@@ -95,20 +95,7 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-    let planets = addDestinationInfo(document);
     let randomPlanet = planets[Math.floor(Math.random() * planets.length)];
-
-    planets.innerHTML = 
-    `<h2>Mission Destination</h2>
-    <ol>
-        <li>Name: ${randomPlanet.name} </li>
-        <li>Diameter: ${randomPlanet.diameter} </li>
-        <li>Star: ${randomPlanet.star}</li>
-        <li>Distance from Earth: ${randomPlanet.distance} </li>
-        <li>Number of Moons: ${randomPlanet.moons} </li>
-    </ol>
-    <img src="${randomPlanet.image}">` 
-
     return randomPlanet;
 }
 
